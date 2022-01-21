5aa537918206c3ea3aeb3570d8b8af6b9b44e4d0
mopeps <lucagiabb@gmail.com>
2022-01-21 17:15:03 -0300

    Executing SQL statements

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,27 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/mopeps/snippetbox/pkg/models/pgql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog *log.Logger
+	snippets *pgql.SnippetModel
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
-	flag.Parse()
+	flag.Parse()   
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	
+	// To keep the main() function tidy I've put the code for creating a connection
+	// pool into the separate openDB() funciton below. We pass openDB() the dsn
+	// from the command-line flag.
+	db, err := openDB()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 	app := &application{
 		errorLog: errorLog,
 		infoLog: infoLog,
+		snippets: &pgql.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server {
@@ -31,6 +45,18 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB() (*sql.DB, error) {
+	db, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }

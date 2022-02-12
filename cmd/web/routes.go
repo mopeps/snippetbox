b99b58badc74a9551a5d97ac72d2180035958ff8
mopeps <lucagiabb@gmail.com>
2022-02-12 18:48:51 -0300

    RESTful Routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"github.com/justinas/alice"
+)
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
+	// Create a middleware chain containing our 'standar' middleware
+	// which will be used for every request our application receives.
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -11,6 +17,9 @@ func (app *application) routes() *http.ServeMux {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	
-	return mux
+	// Pass the servemux as the 'next' parameter to the secureHeaders middleware
+	// Because secureHeaders is just a function, and the function returns a 
+	// http.Handler we don't need to do anything else
+	return standardMiddleware.Then(mux)
 }
 
